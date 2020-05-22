8f48df86ffd22a20ee86370bdcacccc2ca998255
ChernomorYegor <chernomor.yegor@gmail.com>
2020-05-22 15:05:57 +0300

    Updates

---
+++ precache-manifest.e09a6149ff90a2b6a1963c6f7e0d729a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf5ae1dfea87e56c9fa725319442490",
+    "url": "/react-counter-redux-saga/index.html"
+  },
+  {
+    "revision": "a56763c76d7e621cb14b",
+    "url": "/react-counter-redux-saga/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "591e940ad8f70a8a1bef",
+    "url": "/react-counter-redux-saga/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a56763c76d7e621cb14b",
+    "url": "/react-counter-redux-saga/static/js/2.c269bf7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-counter-redux-saga/static/js/2.c269bf7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "591e940ad8f70a8a1bef",
+    "url": "/react-counter-redux-saga/static/js/main.b07776dd.chunk.js"
+  },
+  {
+    "revision": "0864664f07da125d274f",
+    "url": "/react-counter-redux-saga/static/js/runtime-main.3ed3788a.js"
+  }
+]);
